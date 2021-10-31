9235f1a5425bc1bba036b2ee5b8bc060a9fff40d
Divyansh Kushwaha <kushwaha.divyansh.dxn@gmail.com>
2021-10-31 09:07:58 +0530

    modules

---
+++ 22Modules/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Hello from module")
+	greeter()
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
+
+func greeter() {
+	fmt.Println("Hey there!")
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to golang</h1>"))
+}
